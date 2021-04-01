830efbe1f055c269a7ca97a76b69a7a46d3bf6fb
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-04-01 19:52:36 -0300

    added command pattern

---
+++ JavaScript/Behavioral/Command.js
@@ -0,0 +1,37 @@
+/* Command is a behavioral design pattern that turns a 
+request into a stand-alone object that contains all 
+information about the request. This transformation lets 
+you pass requests as a method arguments, delay or queue 
+a request’s execution, and support undoable operations. */
+
+// the creation of the command function which can be called
+// from diferent interfaces
+function Command(data) {
+    console.log(data);
+}
+
+class Button {
+    save(data) {
+        Command(data);
+    }
+}
+
+class MenuItem {
+    save(data) {
+        Command(data);
+    }
+}
+
+class Shortcut {
+    save(data) {
+        Command(data);
+    }
+}
+
+let button = new Button();
+let menuItem = new MenuItem();
+let shortcut = new Shortcut();
+
+button.save('Button');
+menuItem.save('MenuItem');
+Shortcut.save('Shortcut');
